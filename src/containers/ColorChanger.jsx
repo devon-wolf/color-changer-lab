42e5eaea6b21143fa6dfd504a43abaeb2efca05e
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-10 16:57:11 -0700

    Add baby meme on color repeats, need to figure out scaling

--- src/containers/ColorChanger.jsx
+++ src/containers/ColorChanger.jsx
@@ -15,7 +15,10 @@ export default class ColorChanger extends Component {
 			'#4C1C00'
 		];
 
-		return colorOptions[Math.floor(Math.random() * colorOptions.length)];
+		const newColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
+		
+		if (newColor !== this.state.color) return newColor;
+		else return 'url("https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg")'; 
 	}
 
 	changeColor = () => setInterval(() => {
