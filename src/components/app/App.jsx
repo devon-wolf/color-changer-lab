b0336a290e2d9bf61afe25c901d5c58c8eedeeba
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-10 16:16:26 -0700

    Initial square setup

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import ColoredSquare from '../display/ColoredSquare';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <ColoredSquare color="#FFFFFF" />;
 }
---
+++ src/components/display/ColoredSquare.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import style from './square.css';
+
+const ColoredSquare = ({ color }) => (
+	<div className={style.square} style={{backgroundColor: color}}></div>
+);
+
+ColoredSquare.propTypes = {
+	color: PropTypes.string.isRequired
+};
+
+export default ColoredSquare;
