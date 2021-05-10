e253a9ebdca3e959679602a9933cab841e6ee8a8
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-10 16:36:37 -0700

    Add color change functionality

--- src/containers/ColorChanger.jsx
+++ src/containers/ColorChanger.jsx
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 import ColoredSquare from '../components/display/ColoredSquare';
 
 export default class ColorChanger extends Component {
+	state = {
+		color: '#FFFFFF'
+	}
+
+	randomColor = () => {
+		const colorOptions = [
+			'#320D6D',
+			'#FFBFB7',
+			'#FFD447',
+			'#700353',
+			'#4C1C00'
+		];
+
+		return colorOptions[Math.floor(Math.random() * colorOptions.length)];
+	}
+
+	changeColor = () => setInterval(() => {
+		this.setState({ color: this.randomColor()});
+	}, 1000);
+
+	componentDidMount = () => {
+		this.changeColor();
+	}
+
 	render () {
 		return (
-			<ColoredSquare color="#FF00F0" />
+			<ColoredSquare color={this.state.color} />
 		);
 	}
 }
